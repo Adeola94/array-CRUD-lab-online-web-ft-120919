35936d3cf4ffa01495dfa461590b4d191a00943a
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-19 05:58:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
    array = ["wow", "I", "am", "really", "learning"]
-   array.push(array, element)
+   array.push("array", "element")
 end
 
 def add_element_to_start_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-   array.unshift(array, element)
+   array.unshift("array", "element")
 end
 
 def remove_element_from_end_of_array(array)
